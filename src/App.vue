f87358d09b2280b99f9a3068d09f6defe7a41265
lee <233liyu@gmail.com>
2019-03-26 15:44:25 -0400

    introduce to modal

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,39 @@
 <template>
-  <div id="app" style="top:30px; bottom:0px; position:absolute; width:100%;">
+  <div id="app">
     <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
-    <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
-    <vue-waterfall-easy
-      :imgsArr="imgsArr"
-      @scrollReachBottom="getData"
-      @click="clickFn"
-      linkRange="img"
-    >
-      <div slot-scope="props" style="display:flex; flex-flow: column; word-break: break-word">
-        <div class="card-body">
-          <h5 class="card-title">第{{props.index+1}}张图片</h5>
-          <p class="card-text">{{props.value.info}}</p>
+    <!-- nav bar -->
+    <NavBar title="Kaleido Scope"></NavBar>
+
+    <!-- water fall content -->
+    <div style="top:56px; bottom:0px; position:absolute; width:100%;">
+      <vue-waterfall-easy
+        :imgsArr="imgsArr"
+        @scrollReachBottom="getData"
+        @click="clickFn"
+        linkRange="custom"
+      >
+        <div slot-scope="props" class="slot_tab" style>
+          <div class="card-body">
+            <h5 class="card-title">第{{ props.index + 1 }}张图片</h5>
+            <p class="card-text">{{ props.value.info }}</p>
+          </div>
         </div>
-      </div>
-    </vue-waterfall-easy>
+      </vue-waterfall-easy>
+    </div>
+
+    <newsModal ref="news_modal"></newsModal>
+    <!-- end of water fall wrapper -->
   </div>
+  <!-- end of app -->
 </template>
 
 <script>
-// import HelloWorld from "./components/HelloWorld.vue";
+import NavBar from "./components/NavBar.vue";
+import newsModal from "./components/Modal.vue";
+
 import vueWaterfallEasy from "vue-waterfall-easy";
 import axios from "axios";
+
 export default {
   name: "app",
   data() {
@@ -31,7 +43,9 @@ export default {
     };
   },
   components: {
-    vueWaterfallEasy
+    vueWaterfallEasy,
+    NavBar,
+    newsModal
   },
   methods: {
     getData() {
@@ -42,7 +56,16 @@ export default {
         this.group++;
       });
     },
-    clickFn() {}
+    clickFn(event, { index, value }) {
+      // Prevent a tag jump
+      event.preventDefault();
+      // Do it only when you click on the image
+      if (event.target.tagName.toLowerCase() == "img") {
+        // console.log("img clicked", index, value);
+        // alert(value.href);
+        this.$refs.news_modal.toggleModal(value.href);
+      }
+    }
   },
   created() {
     this.getData();
@@ -51,7 +74,5 @@ export default {
 </script>
 
 <style>
-#app {
-  min-height: 300px;
-}
+@import url(./assets/css/main.css);
 </style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,116 @@
+<template>
+  <b-modal ref="myModalRef" v-bind:title="news_title" hide-footer size="lg">
+    <div class="d-block text-center">
+      <h3>Hello From My Modal! {{ news_title }}</h3>
+      <footer class="blockquote-footer">
+        Someone famous in
+        <cite title="Source Title">Source Title</cite>
+      </footer>
+      <p>{{ news_content }}</p>
+      <p>{{ href }}</p>
+      <div class="ru">
+        <div id="circle"></div>
+
+        <span class="badge badge-primary badge-pill">Primary</span>
+        <span class="badge badge-primary badge-pill">Primary</span>
+        <span class="badge badge-primary badge-pill">Primary</span>
+        <span class="badge badge-primary badge-pill">Primary</span>
+        <span class="badge badge-primary badge-pill">Primary</span>
+        <span class="badge badge-primary badge-pill">Primary</span>
+        <span class="badge badge-primary badge-pill">Primary</span>
+        <span class="badge badge-primary badge-pill">Primary</span>
+        <span class="badge badge-primary badge-pill">Primary</span>
+        <span class="badge badge-primary badge-pill">Primary</span>
+        <span class="badge badge-primary badge-pill">Primary</span>
+        <span class="badge badge-primary badge-pill">Primary</span>
+        <span class="badge badge-primary badge-pill">Primary</span>
+        <span class="badge badge-primary badge-pill">Primary</span>
+        <span class="badge badge-primary badge-pill">Primary</span>
+        <span class="badge badge-primary badge-pill">Primary</span>
+        <span class="badge badge-primary badge-pill">Primary</span>
+        <span class="badge badge-primary badge-pill">Primary</span>
+        <span class="badge badge-primary badge-pill">Primary</span>
+        <span class="badge badge-primary badge-pill">Primary</span>
+        <span class="badge badge-primary badge-pill">Primary</span>
+        <span class="badge badge-primary badge-pill">Primary</span>
+        <span class="badge badge-primary badge-pill">Primary</span>
+        <span class="badge badge-primary badge-pill">Primary</span>
+        <span class="badge badge-primary badge-pill">Primary</span>
+        <span class="badge badge-primary badge-pill">Primary</span>
+        <span class="badge badge-primary badge-pill">Primary</span>
+        <span class="badge badge-primary badge-pill">Primary</span>
+        <span class="badge badge-primary badge-pill">Primary</span>
+        <span class="badge badge-primary badge-pill">Primary</span>
+        <span class="badge badge-primary badge-pill">Primary</span>
+        <span class="badge badge-primary badge-pill">Primary</span>
+        <span class="badge badge-primary badge-pill">Primary</span>
+        <span class="badge badge-primary badge-pill">Primary</span>
+        <span class="badge badge-primary badge-pill">Primary</span>
+        <span class="badge badge-primary badge-pill">Primary</span>
+        <span class="badge badge-primary badge-pill">Primary</span>
+        <span class="badge badge-primary badge-pill">Primary</span>
+        <span class="badge badge-primary badge-pill">Primary</span>
+        <span class="badge badge-primary badge-pill">Primary</span>
+        <span class="badge badge-primary badge-pill">Primary</span>
+        <span class="badge badge-primary badge-pill">Primary</span>
+        <span class="badge badge-primary badge-pill">Primary</span>
+
+      </div>
+    </div>
+    <b-button class="mt-3" variant="outline-danger" block @click="hideModal">Close Me</b-button>
+    <!-- <b-button class="mt-2" variant="outline-warning" block @click="toggleModal">Toggle Me</b-button> -->
+  </b-modal>
+</template>
+
+<script>
+export default {
+  name: "newsModal",
+  methods: {
+    showModal() {
+      this.$refs.myModalRef.show();
+    },
+    hideModal() {
+      this.$refs.myModalRef.hide();
+    },
+    toggleModal(value) {
+      // We pass the ID of the button that we want to return focus to when
+      // the modal has hidden
+      this.$refs.myModalRef.toggle("#toggleBtn");
+      this.href = value;
+      //   this.$refs.myModalRef.$props.title = "hahahahha";
+    }
+  },
+  data: function() {
+    return {
+      news_title: "News title",
+      news_content:
+        "<b-modal>, by default, has an OK and Cancel buttons in the footer. These buttons can be customized by setting various props on the component. You can customize the size of the buttons, disable buttons, hide the Cancel button (i.e. OK Only), choose a variant (e.g. danger for a red OK button) using the ok-variant and cancel-variant props, and provide custom button content using the ok-title and cancel-title props, or using the named slots modal-ok and modal-cancel.\n<b-modal> supports close on ESC (enabled by default), close on backdrop click (enabled by default), and the X close button in the header (enabled by default). These features may be disabled by setting the the props no-close-on-esc, no-close-on-backdrop, and hide-header-close respectively.\nYou can override the modal title via the named slot modal-title, override the header completely via the modal-header slot, and override the footer completely via the modal-footer slot.\nNote: when using the modal-footer slot, the default OK and Cancel buttons will not be present. Also, if you use the modal-header slot, the default header X close button will not be present, nor can you use the modal-title slot.",
+      href: String
+    };
+  }
+};
+</script>
+
+<style>
+#circle {
+  width: 200px;
+  height: 200px;
+  float: left;
+  background: red;
+  border-radius: 50%;
+  -webkit-shape-outside: circle();
+  shape-outside: circle();
+}
+
+.badge.badge-pill{
+    font-size: 1.2em;
+    margin-bottom: 0.5em;
+}
+
+/* .ru{
+    width: 500px;
+    height: 500px;
+    border-radius: 20%;
+    background-color: antiquewhite;
+} */
+</style>
